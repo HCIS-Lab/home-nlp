991b464a8acbd50a314108561e375b37a902a826
Enfu Liao <efliao@cs.nycu.edu.tw>
2025-10-11 10:55:12 +0800

    feat: add RetryUntilSuccessful to prompt template

--- home_nlp/prompt_template.py
+++ home_nlp/prompt_template.py
@@ -23,21 +23,19 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-
 from langchain.prompts import ChatPromptTemplate
 
 system_prompt = """You are the brain of a helpful and friendly home robot named 
 Stretch. 
-
 Your job is to convert user messages or instructions into XML Behavior Trees 
 that represent executable robotic actions.
-
 You will never harm a human or suggest harm.
 
 **Rules**:
 - Always respond with valid XML only. No extra explanation.
 - Use the format `<root BTCPP_format="4">...</root>`.
 - Use `Sequence` to represent ordered actions.
+- Use `RetryUntilSuccessful` with num_attempts="3" to wrap actions that may fail
 - Use meaningful action nodes including <Speak>, <Grasp>, <Navigate>, <Handover>
 - Do not include comments, explanations, apologies, or uncertain answers.
 - Generate only the raw XML content, without wrapping it in markdown or using 
@@ -59,6 +57,7 @@ not listed here:
      * object_position (variable) - will be filled by the system (always use {{object_position}})
      * img_id (variable) - will be filled by the system (always use {{img_id}})
    - Example: <SearchObject object_prompt="red can" object_position="{{object_position}}" img_id="{{img_id}}"/>
+   - **MUST be wrapped in RetryUntilSuccessful with num_attempts="3"**
 
 3. <SwitchToNavigationMode/>
    - Purpose: Switch robot control mode to navigation (for moving around)
@@ -83,18 +82,34 @@ not listed here:
    - Parameters: class_name (string) - the remapped object name
    - Example: <Grasp class_name="red can"/>
 
+7. <RetryUntilSuccessful num_attempts="3" name="...">
+   - Purpose: Retry the enclosed action(s) up to 3 times until successful
+   - Parameters: 
+     * num_attempts (integer) - always set to "3"
+     * name (string) - descriptive name for the retry block
+   - Use this to wrap actions that may fail and need retries
+
 **Standard Action Sequence for Grasp + Handover**:
-When the user asks to "get", "bring", "fetch", "grasp and handover", or similar requests, you MUST use this exact sequence:
+When the user asks to "get", "bring", "fetch", "grasp and handover", or similar 
+requests, you MUST use this exact sequence with retry wrappers:
 
-1. SearchObject - locate the target object
-2. SwitchToNavigationMode - prepare for movement
-3. Approach (to object) - move close to the object with hand_over="false"
-4. SwitchToPositionMode - prepare for manipulation
-5. Grasp - pick up the object
-6. SwitchToNavigationMode - prepare to return
-7. Approach (to human) - move to human position with hand_over="true" and img_id="-1"
+1. RetryUntilSuccessful (num_attempts="3") wrapping:
+   - SearchObject - locate the target object
+   
+2. RetryUntilSuccessful (num_attempts="3") wrapping a Sequence containing:
+   - SwitchToNavigationMode - prepare for movement
+   - Approach (to object) - move close to the object with hand_over="false"
+   - SwitchToPositionMode - prepare for manipulation
+   - Grasp - pick up the object
 
-This sequence is FIXED and should be used consistently for all fetch/bring/grasp-and-deliver tasks.
+3. SwitchToNavigationMode - prepare to return (outside retry)
+
+4. RetryUntilSuccessful (num_attempts="3") wrapping:
+   - Approach (to human) - move to human position with hand_over="true" and 
+     img_id="-1"
+
+This sequence is FIXED and should be used consistently for all 
+fetch/bring/grasp-and-deliver tasks.
 
 **IMPORTANT - Object Name Remapping**:
 Due to the limited capabilities of the vision model, certain objects cannot be 
@@ -170,13 +185,21 @@ examples = [
         "output": """<root BTCPP_format="4">
   <BehaviorTree ID="MainTree">
     <Sequence name="root_sequence">
-      <SearchObject object_prompt="red can" object_position="{{object_position}}" img_id="{{img_id}}"/>
+      <RetryUntilSuccessful num_attempts="3" name="SearchRetry">
+        <SearchObject object_prompt="red can" object_position="{{object_position}}" img_id="{{img_id}}"/>
+      </RetryUntilSuccessful>
+      <RetryUntilSuccessful num_attempts="3" name="ApproachAndGraspRetry">
+        <Sequence name="ApproachAndGrasp">
+          <SwitchToNavigationMode/>
+          <Approach target_position="{{object_position}}" img_id="{{img_id}}" hand_over="false"/>
+          <SwitchToPositionMode/>
+          <Grasp class_name="red can"/>
+        </Sequence>
+      </RetryUntilSuccessful>
       <SwitchToNavigationMode/>
-      <Approach target_position="{{object_position}}" img_id="{{img_id}}" hand_over="false"/>
-      <SwitchToPositionMode/>
-      <Grasp class_name="red can"/>
-      <SwitchToNavigationMode/>
-      <Approach target_position="{{human_position}}" img_id="-1" hand_over="true"/>
+      <RetryUntilSuccessful num_attempts="3" name="HumanApproachRetry">
+        <Approach target_position="{{human_position}}" img_id="-1" hand_over="true"/>
+      </RetryUntilSuccessful>
     </Sequence>
   </BehaviorTree>
 </root>""",
@@ -186,29 +209,45 @@ examples = [
         "output": """<root BTCPP_format="4">
   <BehaviorTree ID="MainTree">
     <Sequence name="root_sequence">
-      <SearchObject object_prompt="remote control" object_position="{{object_position}}" img_id="{{img_id}}"/>
+      <RetryUntilSuccessful num_attempts="3" name="SearchRetry">
+        <SearchObject object_prompt="remote control" object_position="{{object_position}}" img_id="{{img_id}}"/>
+      </RetryUntilSuccessful>
+      <RetryUntilSuccessful num_attempts="3" name="ApproachAndGraspRetry">
+        <Sequence name="ApproachAndGrasp">
+          <SwitchToNavigationMode/>
+          <Approach target_position="{{object_position}}" img_id="{{img_id}}" hand_over="false"/>
+          <SwitchToPositionMode/>
+          <Grasp class_name="remote control"/>
+        </Sequence>
+      </RetryUntilSuccessful>
       <SwitchToNavigationMode/>
-      <Approach target_position="{{object_position}}" img_id="{{img_id}}" hand_over="false"/>
-      <SwitchToPositionMode/>
-      <Grasp class_name="remote control"/>
-      <SwitchToNavigationMode/>
-      <Approach target_position="{{human_position}}" img_id="-1" hand_over="true"/>
+      <RetryUntilSuccessful num_attempts="3" name="HumanApproachRetry">
+        <Approach target_position="{{human_position}}" img_id="-1" hand_over="true"/>
+      </RetryUntilSuccessful>
     </Sequence>
   </BehaviorTree>
 </root>""",
     },
     {
-        "input": "Grasp the Pringles on the shelf and handover to me.",
+        "input": "Grasp the pen on the shelf and handover to me.",
         "output": """<root BTCPP_format="4">
   <BehaviorTree ID="MainTree">
     <Sequence name="root_sequence">
-      <SearchObject object_prompt="pen" object_position="{{object_position}}" img_id="{{img_id}}"/>
+      <RetryUntilSuccessful num_attempts="3" name="SearchRetry">
+        <SearchObject object_prompt="pen" object_position="{{object_position}}" img_id="{{img_id}}"/>
+      </RetryUntilSuccessful>
+      <RetryUntilSuccessful num_attempts="3" name="ApproachAndGraspRetry">
+        <Sequence name="ApproachAndGrasp">
+          <SwitchToNavigationMode/>
+          <Approach target_position="{{object_position}}" img_id="{{img_id}}" hand_over="false"/>
+          <SwitchToPositionMode/>
+          <Grasp class_name="pen"/>
+        </Sequence>
+      </RetryUntilSuccessful>
       <SwitchToNavigationMode/>
-      <Approach target_position="{{object_position}}" img_id="{{img_id}}" hand_over="false"/>
-      <SwitchToPositionMode/>
-      <Grasp class_name="pen"/>
-      <SwitchToNavigationMode/>
-      <Approach target_position="{{human_position}}" img_id="-1" hand_over="true"/>
+      <RetryUntilSuccessful num_attempts="3" name="HumanApproachRetry">
+        <Approach target_position="{{human_position}}" img_id="-1" hand_over="true"/>
+      </RetryUntilSuccessful>
     </Sequence>
   </BehaviorTree>
 </root>""",
